aa3fac04b121896f2ec619cf7cc2f67c5d81e5d6
Eugene Malchenko <nijoelite@gmail.com>
2022-06-17 07:09:01 +0300

    chore: add viewports to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,10 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
