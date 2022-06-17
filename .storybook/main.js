7f1e004da22684b35034dd07c0464f71fdd29f8a
Eugene Malchenko <nijoelite@gmail.com>
2022-06-17 09:13:56 +0300

    chore: remove docgen

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    { name: '@storybook/addon-essentials', options: { docs: false } },
     '@storybook/addon-interactions',
   ],
   framework: '@storybook/react',
@@ -13,9 +13,6 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
-  },
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
   },
   webpackFinal: async (config, { configType }) => {
     config.resolve.alias = {
