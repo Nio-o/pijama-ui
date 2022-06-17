8ede3c451dadd4064590c86ca938e902a31ddb61
Eugene Malchenko <nijoelite@gmail.com>
2022-06-17 04:01:51 +0300

    chore: adjust eslint rules

--- src/ui/Button/Button.stories.tsx
+++ src/ui/Button/Button.stories.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+
 import { ComponentMeta, ComponentStory } from '@storybook/react'
+
 import { Button } from 'pijama/ui/Button'
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -9,16 +11,16 @@ export default {
 } as ComponentMeta<typeof Button>
 
 const Template: ComponentStory<typeof Button> = ({ ...args }) => (
-  <Button {...args}>Получить кыарту</Button>
+  <Button {...args}>Получить карту</Button>
 )
 
 export const Playground = Template.bind({})
 Playground.argTypes = {
-  className: { type: 'string', description: 'ClassName' },
+  className: { type: 'string', description: 'Фввz' },
 }
 
 export const ButtonB2C = Template.bind({})
-// ButtonB2C.storyName = "Theme: b2c";
+ButtonB2C.storyName = 'Theme: B2C'
 
 export const ButtonB2B = Template.bind({})
-// ButtonB2B.storyName = "Theme: b2b";
+ButtonB2B.storyName = 'Theme: B2B'
