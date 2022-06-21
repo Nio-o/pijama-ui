c1c81b1bc3183dba3b077a5f1876c13aef3432ba
Eugene Malchenko <nijoelite@gmail.com>
2022-06-21 07:34:20 +0300

    chore(ci): compile bundles to measure sizes

---
+++ infra/bundles.js
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
+const esbuild = require('esbuild')
+const glob = require('fast-glob')
+const cssModulesPlugin = require('./esbuild-css-modules')
+
+const target = 'es6'
+const format = 'esm'
+
+const bundles = glob.sync('./ui/**/bundle.js')
+
+const startBundlesBuild = async () => {
+  return esbuild.build({
+    format,
+    entryPoints: bundles,
+    logLevel: 'info',
+    write: false,
+    outdir: './bundles',
+    outbase: './src',
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    target,
+    plugins: [nodeExternalsPlugin(), cssModulesPlugin()],
+    tsconfig: './tsconfig.build.json',
+  })
+}
+
+startBundlesBuild()
