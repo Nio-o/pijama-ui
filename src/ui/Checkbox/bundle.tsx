6a8b937c14a9cb6f2c500153b104ddf76ee73a22
Nikita <39910442+cain-in-node@users.noreply.github.com>
2022-06-23 00:35:22 +0300

    Checkbox (#16)
    made checkbox

---
+++ src/ui/Checkbox/bundle.tsx
@@ -0,0 +1,7 @@
+import type { ComponentProps } from 'react'
+
+import { Checkbox as BaseCheckbox } from './Checkbox'
+
+export const Checkbox = BaseCheckbox
+
+export type CheckboxProps = ComponentProps<typeof Checkbox>
