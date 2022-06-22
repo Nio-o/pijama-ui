b922c83defbb7c79cbfae3ddd5b86eb86ecf0b85
Evgenii Malchenko <nijoelite@gmail.com>
2022-06-22 15:59:50 +0300

    feat(button): variant simple

--- src/ui/Button/bundle.tsx
+++ src/ui/Button/bundle.tsx
@@ -6,9 +6,13 @@ import { withSizeL } from './_size/Button_size_l'
 import { withSizeM } from './_size/Button_size_m'
 import { withSizeS } from './_size/Button_size_s'
 import { withVariantBrand } from './_variant/Button_variant_brand'
+import { withVariantSimple } from './_variant/Button_variant_simple'
 
 import { Button as BaseButton } from './Button'
 
-export const Button = compose(composeU(withSizeS, withSizeM, withSizeL), composeU(withVariantBrand))(BaseButton)
+export const Button = compose(
+  composeU(withSizeS, withSizeM, withSizeL),
+  composeU(withVariantBrand, withVariantSimple),
+)(BaseButton)
 
 export type ButtonProps = ComponentProps<typeof Button>
