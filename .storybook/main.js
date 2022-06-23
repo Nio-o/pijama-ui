61df38a8dbcb5f063c9da03f533046e99fab09e4
Evgenii Malchenko <nijoelite@gmail.com>
2022-06-23 09:25:50 +0300

    feat(docs): add basic mdx docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    { name: '@storybook/addon-essentials', options: { docs: false } },
+    '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-storysource',
   ],
