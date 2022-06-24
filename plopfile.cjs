f6fb03bde149327ee3fd3b208c41a7c4a24b17a5
Evgenii Malchenko <nijoelite@gmail.com>
2022-06-24 21:49:24 +0300

    chore: add mods generator, fix style

--- plopfile.cjs
+++ plopfile.cjs
@@ -1,3 +1,5 @@
+const notEmptyString = (input) => (!input.trim() ? 'Empty string is not allowed' : true)
+
 /**
  *
  * @param {import('plop').NodePlopAPI} plop
@@ -9,6 +11,7 @@ module.exports = function (plop) {
       {
         type: 'input',
         name: 'name',
+        validate: notEmptyString,
       },
     ],
     actions: [
@@ -17,6 +20,40 @@ module.exports = function (plop) {
         destination: 'src/ui/{{pascalCase name}}',
         base: 'infra/generators/new-block',
         templateFiles: ['infra/generators/new-block/**/*.hbs'],
+        abortOnFail: true,
+      },
+    ],
+  })
+
+  plop.setGenerator('Modifier', {
+    description: 'Create new modifier for block',
+    prompts: [
+      {
+        type: 'input',
+        name: 'blockName',
+        message: 'Block name',
+        validate: notEmptyString,
+      },
+      {
+        type: 'input',
+        name: 'modName',
+        message: 'Modifier name',
+        validate: notEmptyString,
+      },
+      {
+        type: 'input',
+        name: 'modValue',
+        message: 'Modifier value',
+        validate: notEmptyString,
+      },
+    ],
+    actions: [
+      {
+        type: 'addMany',
+        destination: 'src/ui/{{pascalCase blockName}}',
+        base: 'infra/generators/new-mod',
+        templateFiles: ['infra/generators/new-mod/**/*.hbs'],
+        abortOnFail: true,
       },
     ],
   })
